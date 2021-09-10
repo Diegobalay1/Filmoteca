bfb2a9480f3d854a92eaf3e56caab8da0b612ef5
balay <dlolhd@gmail.com>
2021-09-10 09:36:24 +0200

    Commit de añadidos unos strings, arrays y dimensiones. Actividad principal renombrada a FilmListActivity.kt

--- app/src/main/java/com/diego/kotlin/filmoteca/FilmListActivity.kt
+++ app/src/main/java/com/diego/kotlin/filmoteca/FilmListActivity.kt
@@ -3,9 +3,9 @@ package com.diego.kotlin.filmoteca
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class FilmListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_film_list)
     }
 }

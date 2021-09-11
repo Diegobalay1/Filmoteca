b68a41717373e9e6eaacc6d74343bb74c948b1fc
balay <dlolhd@gmail.com>
2021-09-11 11:08:26 +0200

    Commit de creación actividad 'FilmEditActivity.kt'

--- app/src/main/java/com/diego/kotlin/filmoteca/FilmEditActivity.kt
+++ app/src/main/java/com/diego/kotlin/filmoteca/FilmEditActivity.kt
@@ -1,11 +1,25 @@
 package com.diego.kotlin.filmoteca
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class FilmEditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_film_edit)
+
+        val btnGuardar = findViewById<Button>(R.id.btnEditSave)
+        val btnCancelar = findViewById<Button>(R.id.btnEditCancel)
+
+        btnGuardar.setOnClickListener {
+            setResult(Activity.RESULT_OK)
+            finish()
+        }
+        btnCancelar.setOnClickListener {
+            setResult(Activity.RESULT_CANCELED)
+            finish()
+        }
     }
 }

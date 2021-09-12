b74d35b28b60495f5e35d06900036648944f094e
balay <dlolhd@gmail.com>
2021-09-12 09:17:32 +0200

    Commit de creación clase FilmDataSource.kt la cual nos proporcionará acceso al listado de películas

--- app/src/main/java/com/diego/kotlin/filmoteca/FilmDataSource.kt
+++ app/src/main/java/com/diego/kotlin/filmoteca/FilmDataSource.kt
@@ -4,5 +4,71 @@ package com.diego.kotlin.filmoteca
  * Nos proporciona acceso al listado de películas
  */
 class FilmDataSource {
+    val films: MutableList<Film> = mutableListOf()
+    var f = Film()
 
+    init {
+        f.title = "Mi Película"
+        f.director = "Diego L. Ortiz"
+        f.imageResId = R.mipmap.ic_launcher
+        f.comments = ""
+        f.format = Film.FORMAT_DIGITAL
+        f.genre = Film.GENRE_SCIFI
+        f.imdbUrl = "http://www.imdb.com/title/tt0088763"
+        f.year = 1985
+        films.add(f)
+        regresoAlFuturo()
+        regresoAlFuturo2()
+        regresoAlFuturo3()
+        losCazaFantasmas()
+    }
+
+    fun regresoAlFuturo() {
+        f = Film()
+        f.title = "Regreso al futuro"
+        f.director = "Robert Zemeckis"
+        f.imageResId = R.drawable.regreso_al_futuro
+        f.comments = ""
+        f.format = Film.FORMAT_DIGITAL
+        f.genre = Film.GENRE_SCIFI
+        f.imdbUrl = "http://www.imdb.com/title/tt0088763"
+        f.year = 1985
+        films.add(f)
+    }
+    fun regresoAlFuturo2() {
+        f = Film()
+        f.title = "Regreso al futuro II"
+        f.director = "Robert Zemeckis"
+        f.imageResId = R.drawable.regreso_al_futuro
+        f.comments = ""
+        f.format = Film.FORMAT_DIGITAL
+        f.genre = Film.GENRE_SCIFI
+        f.imdbUrl = "http://www.imdb.com/title/tt0096874"
+        f.year = 1989
+        films.add(f)
+    }
+    fun regresoAlFuturo3() {
+        f = Film()
+        f.title = "Regreso al futuro III"
+        f.director = "Robert Zemeckis"
+        f.imageResId = R.drawable.regreso_al_futuro
+        f.comments = ""
+        f.format = Film.FORMAT_DIGITAL
+        f.genre = Film.GENRE_SCIFI
+        f.imdbUrl = "http://www.imdb.com/title/tt0099088"
+        f.year = 1990
+        films.add(f)
+    }
+    fun losCazaFantasmas() {
+        f = Film()
+        f.title = "Los cazafantasmas"
+        f.director = "Ivan Reitman"
+        f.imageResId = R.mipmap.ic_launcher
+        f.comments = ""
+        f.format = Film.FORMAT_DIGITAL
+        f.genre = Film.GENRE_COMEDY
+        f.imdbUrl = "http://www.imdb.com/title/tt0087332"
+        f.year = 1984
+        films.add(f)
+    }
 }
--- app/src/main/java/com/diego/kotlin/filmoteca/FilmListActivity.kt
+++ app/src/main/java/com/diego/kotlin/filmoteca/FilmListActivity.kt
@@ -3,14 +3,21 @@ package com.diego.kotlin.filmoteca
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListAdapter
+import android.widget.ListView
 
 class FilmListActivity : AppCompatActivity() {
+    var adapter : ListAdapter? = null
+    var listView: ListView? = null
+    var peliculas = FilmDataSource()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_film_list)
+        //setContentView(R.layout.activity_film_list)
 
-        val btnPeliculaA = findViewById<Button>(R.id.btnPeliculaA)
+        /*val btnPeliculaA = findViewById<Button>(R.id.btnPeliculaA)
         val btnPeliculaB = findViewById<Button>(R.id.btnPeliculaB)
         val btnAcercaDe = findViewById<Button>(R.id.btnAcercaDe)
 
@@ -27,6 +34,11 @@ class FilmListActivity : AppCompatActivity() {
         btnAcercaDe.setOnClickListener {
             val intent = Intent(this@FilmListActivity, AboutActivity::class.java)
             startActivity(intent)
-        }
+        }*/
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, peliculas.films)
+        listView = ListView(this)
+        listView?.adapter = adapter
+        setContentView(listView)
     }
 }

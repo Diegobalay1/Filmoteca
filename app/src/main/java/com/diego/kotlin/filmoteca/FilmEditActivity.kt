43752661ee9d4a261a212ef9ca665d58f26f0aee
balay <dlolhd@gmail.com>
2021-09-15 09:56:35 +0200

    Commit de pasamos el indice de la pelicula mostrada con sus datos en 'FilmDataActivity' a 'FilmEditActivity', donde mostrará la película en cuestión a editar.

--- app/src/main/java/com/diego/kotlin/filmoteca/FilmEditActivity.kt
+++ app/src/main/java/com/diego/kotlin/filmoteca/FilmEditActivity.kt
@@ -3,12 +3,21 @@ package com.diego.kotlin.filmoteca
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.widget.*
 
 class FilmEditActivity : AppCompatActivity() {
+
+    var filmIndex = 0
+
+    companion object {
+        const val EXTRA_FILM_INDEX = "EXTRA_FILM_INDEX"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_film_edit)
+
+        filmIndex = intent.getIntExtra(EXTRA_FILM_INDEX, -1)
 
         val btnGuardar = findViewById<Button>(R.id.btnEditSave)
         val btnCancelar = findViewById<Button>(R.id.btnEditCancel)
@@ -21,8 +30,46 @@ class FilmEditActivity : AppCompatActivity() {
             setResult(Activity.RESULT_CANCELED)
             finish()
         }
+
+        loadFilmData()
     }
-    companion object {
-        const val EXTRA_FILM_INDEX = "EXTRA_FILM_INDEX"
+
+    private fun loadFilmData() {
+        val etTitle = findViewById<EditText>(R.id.editTextTitle)
+        val etDirector = findViewById<TextView>(R.id.editTextDirector)
+        val etAnyo = findViewById<TextView>(R.id.editTextYear)
+        val etComentarios = findViewById<TextView>(R.id.editTextComments)
+        val etImdb = findViewById<TextView>(R.id.editTextURL)
+        val spinnerFormato = findViewById<Spinner>(R.id.spinnerFormat)
+        val spinnerGenero = findViewById<Spinner>(R.id.spinnerGenre)
+        val imgPoster = findViewById<ImageView>(R.id.imageViewCartel)
+
+        val film = FilmDataSource.films[filmIndex]
+
+        etTitle.setText(film.title)
+        etDirector.text = film.director
+        etAnyo.text = "" + film.year
+        etComentarios.text = film.comments
+        etImdb.text = film.imdbUrl
+        spinnerFormato.setSelection(film.format, true)
+        spinnerGenero.setSelection(film.genre, false)
+        imgPoster.setImageResource(film.imageResId)
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

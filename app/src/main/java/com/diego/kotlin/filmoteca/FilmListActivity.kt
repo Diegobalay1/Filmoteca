6b46a19dc9d9106c9bea240d88cbf35844d2fccf
balay <dlolhd@gmail.com>
2021-11-04 09:23:44 +0100

    Commit de Borrado multiple de películas. Implementado en FilmListActivity la posibilidad de realizar un borrado múltiple de películas mediante la activación de action mode. Utilizamos para ello la implementación  basada en ListView, en lugar de la basada en RecyclerView.

--- app/src/main/java/com/diego/kotlin/filmoteca/FilmListActivity.kt
+++ app/src/main/java/com/diego/kotlin/filmoteca/FilmListActivity.kt
@@ -3,8 +3,11 @@ package com.diego.kotlin.filmoteca
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ActionMode
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.AbsListView
+import android.widget.ArrayAdapter
 import android.widget.ListAdapter
 import android.widget.ListView
 import androidx.recyclerview.widget.DefaultItemAnimator
@@ -12,13 +15,13 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class FilmListActivity : AppCompatActivity() {
-    //var adapter : ListAdapter? = null
-    //var listView: ListView? = null
+    var adapter : ArrayAdapter<Film>? = null
+    var listView: ListView? = null
     //var peliculas = FilmDataSource()
 
-    var recyclerView: RecyclerView? = null
+    /*var recyclerView: RecyclerView? = null
     var layoutManager: RecyclerView.LayoutManager? = null
-    var adapter: FilmAdapterRecyclerView? = null
+    var adapter: FilmAdapterRecyclerView? = null*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,18 +48,60 @@ class FilmListActivity : AppCompatActivity() {
 
         //adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, peliculas.films)
         //adapter = FilmAdapterListView(this, R.layout.item_lenguaje, peliculas.films)
-        /*adapter = FilmAdapterListView(this, R.layout.item_film, FilmDataSource.films)
+        adapter = FilmAdapterListView(this, R.layout.item_film, FilmDataSource.films)
         listView = ListView(this)
         listView?.adapter = adapter
-        setContentView(listView)
+
 
         listView?.setOnItemClickListener { parent, view, position, id ->
             val intent = Intent(this@FilmListActivity, FilmDataActivity::class.java)
             intent.putExtra(FilmDataActivity.EXTRA_FILM_INDEX, position)
             startActivity(intent)
-        }*/
+        }
 
-        recyclerView = findViewById(R.id.lista_films)
+        listView?.choiceMode = ListView.CHOICE_MODE_MULTIPLE_MODAL
+        listView?.setMultiChoiceModeListener(
+            object : AbsListView.MultiChoiceModeListener {
+                override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+                    val inflater = mode?.menuInflater
+                    inflater?.inflate(R.menu.film_list_contextual_menu, menu)
+                    return true
+                }
+
+                override fun onPrepareActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+                    return false
+                }
+
+                override fun onActionItemClicked(mode: ActionMode?, item: MenuItem?): Boolean {
+                    return when (item?.itemId) {
+                        R.id.miBorrar -> {
+                            borrarItemsSeleccionados()
+                            mode?.finish()
+                            true
+                        }
+                        else -> false
+                    }
+                }
+
+                override fun onDestroyActionMode(mode: ActionMode?) {
+
+                }
+
+                override fun onItemCheckedStateChanged(
+                    mode: ActionMode?,
+                    position: Int,
+                    id: Long,
+                    checked: Boolean
+                ) {
+
+                }
+
+            }
+        )
+
+        setContentView(listView)
+
+        /*recyclerView = findViewById(R.id.lista_films)
 
         layoutManager = LinearLayoutManager(this)
         recyclerView?.layoutManager = layoutManager
@@ -68,9 +113,23 @@ class FilmListActivity : AppCompatActivity() {
             val intent = Intent(this@FilmListActivity, FilmDataActivity::class.java)
             intent.putExtra(FilmDataActivity.EXTRA_FILM_INDEX, position)
             startActivity(intent)
-        }
+        }*/
 
     }//end onCreate()
+
+    private fun borrarItemsSeleccionados() {
+        listView?.let {
+            val indices = it.checkedItemPositions
+            val toDelete: MutableList<Film> = ArrayList()
+            for (i in 0 until indices.size()) {
+                if (indices.valueAt(i)) {
+                    toDelete.add(FilmDataSource.films[indices.keyAt(i)])
+                }
+            }
+            FilmDataSource.films.removeAll(toDelete)
+            adapter?.notifyDataSetChanged()
+        }
+    }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         super.onCreateOptionsMenu(menu)
@@ -99,14 +158,14 @@ class FilmListActivity : AppCompatActivity() {
     }
 
     private fun nuevaPelicula() {
-        /*val f = Film()
-        FilmDataSource.films.add(f)
-        adapter?.notifyDataSetChanged()*/
         val f = Film()
+        FilmDataSource.films.add(f)
+        adapter?.notifyDataSetChanged()
+        /*val f = Film()
         f.title = "New Movie"
         f.imageResId = R.mipmap.ic_launcher
         FilmDataSource.films.add(f)
-        adapter?.notifyItemInserted(FilmDataSource.films.size - 1)
+        adapter?.notifyItemInserted(FilmDataSource.films.size - 1)*/
     }
 
 }
